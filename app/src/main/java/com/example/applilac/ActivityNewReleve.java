ec6f4fe8f22852d01dd6f536a4c18e9664a2cb8e
sereincommewilliam <atignard@la-joliverie.com>
2020-10-14 12:30:58 +0200

    Menu

---
+++ app/src/main/java/com/example/applilac/ActivityNewReleve.java
@@ -0,0 +1,14 @@
+package com.example.applilac;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityNewReleve extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newreleve);
+
+    }
+}
