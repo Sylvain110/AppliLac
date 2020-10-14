3636a2c501f58d3d7b84fbf14a6a65c5e6e11aa7
SAGNA <skimbangui@la-joliverie.com>
2020-10-14 23:33:23 +0200

    Fonctionnabilité des boutons

--- app/src/main/java/com/example/applilac/MainActivity.java
+++ app/src/main/java/com/example/applilac/MainActivity.java
@@ -1,13 +1,87 @@
 package com.example.applilac;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity<Bundle> extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(android.os.Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //remplirTableClient();
+        //on associe à un object java de type Button, un widget repéré physiquement par son id
+        Button btnMoyenneReleves = findViewById(R.id.btnAfficherMoyenneReleves);
+        Button btnAfficherReleves = findViewById(R.id.btnAfficherReleves);
+        Button btnNewReleves = findViewById(R.id.btnNewReleve);
+        //on place un écouteur dessus
+        View.OnClickListener ecouteur = new View.OnClickListener() {
+            //on implémente la méthode onclick
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btnAfficherMoyenneReleves:
+                        Intent intent1 = new Intent(MainActivity.this,
+                                ActivityMoyenneReleve.class);
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.btnAfficherReleves:
+                        Intent intent2 = new Intent(MainActivity.this,
+                                ActivityAfficherReleve.class);
+                        startActivity(intent2);
+                        break;
+
+                    case R.id.btnNewReleve:
+                        Intent intent3 = new Intent(MainActivity.this,
+                                ActivityNewReleve.class);
+                        startActivity(intent3);
+                        break;
+                }
+            }
+        };
+        //on affecte au bouton l'écouteur
+        btnMoyenneReleves.setOnClickListener(ecouteur);
+        btnAfficherReleves.setOnClickListener(ecouteur);
+        btnNewReleves.setOnClickListener(ecouteur);
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.menu_MoyenneReleve:
+                Toast.makeText(getApplicationContext(),"ouverture fenêtre Saisir " +
+                        "relevé !", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, ActivityMoyenneReleve.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.menu_AfficherReleve:
+                Toast.makeText(getApplicationContext(), "ouverture fenêtre liste des " +
+                        "clients !", Toast.LENGTH_LONG).show();
+                Intent intent2 = new Intent(MainActivity.this, ActivityAfficherReleve.class);
+                startActivity(intent2);
+                return true;
+
+            case R.id.menu_NewReleve:
+                Toast.makeText(getApplicationContext(), "ouverture fenêtre liste des " +
+                        "relevés !", Toast.LENGTH_LONG).show();
+                Intent intent3 = new Intent(MainActivity.this, ActivityNewReleve.class);
+                startActivity(intent3);
+                return true;
+        }
+        return false;
     }
 }
