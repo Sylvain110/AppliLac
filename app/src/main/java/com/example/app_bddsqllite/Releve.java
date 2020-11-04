1ce7c30c6534494ac127c2a8cbff8612e3f068df
Sylvain110 <sylvainmabit.101101@gmail.com>
2020-11-04 12:17:19 +0100

    oui pq pas

--- app/src/main/java/com/example/app_bddsqllite/Releve.java
+++ app/src/main/java/com/example/app_bddsqllite/Releve.java
@@ -3,49 +3,52 @@ package com.example.app_bddsqllite;
 public class Releve {
     protected String numlac;
     protected String jour;
+    protected String mois;
     protected String heure;
-    protected String qte;
+    protected float temp;
 
     //constructeur paramétré
-    public  Releve (String numlac, String jour, String mois, int heure, float temp){
+    public  Releve (String numlac, String jour, String mois, String heure, float temp){
         super();
-        this.numlac=unnumlac;
-        this.jour=unjour;
-        this.mois=unmois;
-        this.heure=uneheure;
-		this.temp=unetemp;
+        this.numlac=numlac;
+        this.jour=jour;
+        this.mois=mois;
+        this.heure=heure;
+		this.temp=temp;
     }
+
 
     //les accesseurs
     public String getnumlac(){
+
         return numlac;
     }
-    public void setnumlac(String numlac){
-        this.numlac=numlac;
-    }
-    public String getjour(){
+    public String getjour() {
         return jour;
-    }
-    public void setjour(String jour) {
-        this.jour = jour;
-    }
-    public String getheure(){
-        return heure;
-    }
-	
-	public float gettemp(){
-        return temp;
-    }
-
-    //les mutateurs
-    public void setheure(int heure){
-        this.heure=heure;
     }
     public String getmois(){
         return mois;
     }
+    public String getheure(){
+        return heure;
+    }
+	public float gettemp(){
+        return temp;
+    }
+
+
+    //les mutateurs
+    public void setnumlac(String numlac){
+        this.numlac=numlac;
+    }
+    public void setjour(String jour) {
+        this.jour = jour;
+    }
     public void setmois(String mois){
         this.mois=mois;
+    }
+    public void setheure(String heure){
+        this.heure=heure;
     }
 	public void settemp(float temp){
         this.temp=temp;
